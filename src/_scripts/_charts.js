ee3a849f122ed5dd9d9f5a80da6c22a20e410d1b
Tim Henderson <tim.hendo@gmail.com>
2019-03-09 17:17:25 -0800

    added minimap

--- src/_scripts/_charts.js
+++ src/_scripts/_charts.js
@@ -1,54 +1,75 @@
 var d3 = require('d3');
 
+function createChart(el,fieldname) {
+
 var margin = {top: 20, right:20, bottom:20, left:40};
 // Make sure you use the # here!
-var container = d3.select('#county-homicides');
+var container = d3.select(el);
 var containerWidth = container.node().offsetWidth;
 var containerHeight = containerWidth * 0.66;
 var chartWidth = containerWidth - margin.right - margin.left;
 var chartHeight = containerHeight - margin.top - margin.bottom;
 
 var svg = container.append('svg')
-            .attr('width', containerWidth)
-            .attr('height', containerHeight)
-var svg = container.append('svg')
         .attr('width', chartWidth)
         .attr('height', chartHeight)
         .append('g')
             .attr('transform', `translate(${margin.left}, ${margin.top})`)
- var xDomain = annualTotals.map(d => d.year);
 
-    var yDomain = [
-    0,
-    d3.max(annualTotals.map(d => d.homicides_total))
-    ];
-var xScale = d3.scaleBand()
-              .domain(xDomain)
-              .range([0, chartWidth])
-              .padding(0.1);
+var xDomain = annualTotals.map(d => d.year);
 
+  var yDomain = [
+      0,
+      d3.max(annualTotals.map(d => d[fieldname]))
+  ];
+
+  var xScale = d3.scaleBand()
+                .domain(xDomain)
+                .range([0, chartWidth])
+                .padding(0.1);
 var yScale = d3.scaleLinear()
-              .domain(yDomain)
-              .range([chartHeight, 0]);
-var xAxis = d3.axisBottom(xScale);
-var yAxis = d3.axisLeft(yScale)
-              .tickSize(-chartWidth)
-              .ticks(4);
-svg.append("g")
+                .domain(yDomain)
+                .range([chartHeight, 0]);
+
+  var xAxis = d3.axisBottom(xScale);
+  var yAxis = d3.axisLeft(yScale)
+                  .tickSize(-chartWidth)
+                  .ticks(4);
+
+  svg.append("g")
       .attr("class", "x axis")
       .attr("transform", `translate(0,${chartHeight})`)
       .call(xAxis);
 
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
 
-svg.append("g")
+   svg.selectAll('.bar')
+      .data(annualTotals)
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', d => xScale(d.year))
+      .attr('y', d => yScale(d[fieldname]))
+      .attr('width', d => xScale.bandwidth())
+      .attr('height', d => chartHeight - yScale(d[fieldname]))
+      .on('mouseenter', function(d) {
+          // centers the text above each bar
+          var x = xScale(d.year) + xScale.bandwidth() / 2;
+          // the - 5 bumps up the text a bit so it's not directly over the bar
+          var y = yScale(d[fieldname]) - 5;
 
-svg.selectAll('.bar')
-    .data(annualTotals)
-    .enter()
-    .append('rect')
-    .attr('class', 'bar')
-    .attr('x', d => xScale(d.year))
-    .attr('y', d => yScale(d.homicides_total))
-    .attr('width', xScale.bandwidth())
-    .attr('height', d => chartHeight - yScale(d.homicides_total))
-                                              
+          d3.select(this).classed('highlight', true);
+          tooltip.text(d[fieldname])
+              .attr('transform', `translate(${x}, ${y})`)
+      })
+      .on('mouseleave', function(d) {
+          d3.select(this).classed('highlight', false);
+          tooltip.text('');
+      });
+}
+createChart("#county-homicides", "homicides_total")
+createChart("#harvard-park-homicides", "harvard_park_homicides")
+
+
--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -12,3 +12,7 @@ $(() => {
 });
 
 var chart = require('./_charts.js');
+var L = require("leaflet");
+var MiniMap = require('leaflet-minimap');
+L.Icon.Default.imagePath = 'https://unpkg.com/leaflet@1.4.0/dist/images/';
+var map = require("./_map.js");
